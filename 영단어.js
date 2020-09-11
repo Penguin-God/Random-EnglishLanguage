2e22f4f5cee363e75e5598e6b591e0bfd7b7a885
sktt1-win <gkswh4860@naver.com>
2020-09-11 15:50:04 +0900

    뜻 안보고 넘기는거 막기

--- 영단어.js
+++ 영단어.js
@@ -5,21 +5,26 @@ var RandomArray
 
 function RandomWord() 
 {
-    // 배열크기에 맞는 수 랜덤으로 부르고 자르기
-    RandomArray = Math.floor(Math.random() * word.length)
-    var 영단어구분 = word[RandomArray].split(":")
+    if(document.getElementById("뜻").innerText == "뜻" && document.getElementById("word").innerText != "영단어"){
+        console.log("awsfdga")
+    }
+    else{
+        // 배열크기에 맞는 수 랜덤으로 부르고 자르기
+        RandomArray = Math.floor(Math.random() * word.length)
+        var 영단어구분 = word[RandomArray].split(":")
 
-    // 배열 구분 및 html에 값 전달
-    document.getElementById("word").innerHTML = 영단어구분[0]
-    document.getElementById("뜻").innerHTML = "뜻"
+        // 배열 구분 및 html에 값 전달
+        document.getElementById("word").innerHTML = 영단어구분[0]
+        document.getElementById("뜻").innerHTML = "뜻"
 
-    // 이미 한번 본 영단어 삭제
-    word.splice(RandomArray, 1) // .splice(index, 삭제할원소 개수, 치환할 내용)
+        // 이미 한번 본 영단어 삭제
+        word.splice(RandomArray, 1) // .splice(index, 삭제할원소 개수, 치환할 내용)
 
-    // 영단어 다 볼시 문구
-    if(word.length == 0){
-        document.getElementById("word").innerHTML = "영단어를"
-        document.getElementById("뜻").innerHTML = "다 봤습니다"
+        // 영단어 다 볼시 문구
+        if(word.length == 0){
+            document.getElementById("word").innerHTML = "영단어를"
+            document.getElementById("뜻").innerHTML = "다 봤습니다"
+        }
     }
 }
 
