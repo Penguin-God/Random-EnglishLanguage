3875a860c67eafc110132f812d326d27bf58874c
sktt1-win <gkswh4860@naver.com>
2020-09-11 15:40:37 +0900

    뜻확인 함수 구현

--- 영단어.js
+++ 영단어.js
@@ -1,18 +1,21 @@
 // if(1){
 //     document.write(Math.floor(Math.random() * 100));
 // }
-const 영단어원본 = ["one : 1", "two : 2", "three : 3", "four : 4", "five : 5", "six : 6", "seven : 7"]
+//const 영단어원본 = ["one : 1", "two : 2", "three : 3", "four : 4", "five : 5", "six : 6", "seven : 7"]
 var word = ["one : 1", "two : 2", "three : 3", "four : 4", "five : 5", "six : 6", "seven : 7"] // Key가 영단어 value가 뜻
+var number = ["1", "2", "3", "4", "5", "6", "7"]
+
+var RandomArray
 
 function RandomWord() 
 {
     // 배열크기에 맞는 수 랜덤으로 부르고 자르기
-    var RandomArray = Math.floor(Math.random() * word.length)
+    RandomArray = Math.floor(Math.random() * word.length)
     var 영단어구분 = word[RandomArray].split(":")
 
     // 배열 구분 및 html에 값 전달
     document.getElementById("word").innerHTML = 영단어구분[0]
-    document.getElementById("word").innerHTML = 영단어구분[1]
+    document.getElementById("뜻").innerHTML = "뜻"
 
     // 이미 한번 본 영단어 삭제
     word.splice(RandomArray, 1) // .splice(index, 삭제할원소 개수, 치환할 내용)
@@ -37,6 +40,14 @@ function 다시하기()
 
 // 뜻 보여주기 함수 아직 미완 
 function 뜻보여주기(){
-    document.getElementById("뜻").innerHTML = 영단어구분[1]
+    document.getElementById("뜻").innerHTML = number[RandomArray]
+    number.splice(RandomArray, 1)
 }
+
+// $(function(){
+//     $("#뜻보여주기").on("click", function(){
+//         var $target = $("input #뜻");
+//         $target.removeId("뜻");
+//     });
+// });
 
