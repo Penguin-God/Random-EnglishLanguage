a4dd7d84ef3ad0c878997e4c89b5f6eed1572d97
Penguin-God <gkswh4860@naver.com>
2020-09-14 22:01:43 +0900

    단어장 splic형식으로 복귀

--- 영단어.js
+++ 영단어.js
@@ -1,7 +1,8 @@
-var word = ["perserve", "ordinary", "come up with", "method", "rely on", "charm", "lifelong"] // Key가 영단어 value가 뜻
-var number = ["지키다, 간직하다", "평범한", "~을 생각해내다", "방법", "~에 의존하는", "매력, 작은 장식물", "평생의"] // 뜻
+var word = ["perserve : 지키다", "ordinary : 평범한", "come up with : ~을 생각해내다", "method : 방법", "rely on : ~에 의존하는", "charm : 매력, 작은 장식물", "lifelong : 평생의"] // Key가 영단어 value가 뜻
+//var number = ["지키다, 간직하다", "평범한", "~을 생각해내다", "방법", "~에 의존하는", "매력, 작은 장식물", "평생의"] // 뜻
 
 var RandomArray
+var 영단어구분
 
 function RandomWord() 
 {
@@ -11,9 +12,11 @@ function RandomWord()
     else if(document.getElementById("뜻").innerText != "뜻을 확인해주세요"){
         // 배열크기에 맞는 수 랜덤으로 부르고 자르기
         RandomArray = Math.floor(Math.random() * word.length)
+        영단어구분 = word[RandomArray].split(":")
 
         // 배열 구분 및 html에 값 전달
-        document.getElementById("word").innerHTML = word[RandomArray]
+        document.getElementById("word").innerHTML = 영단어구분[0]
+        //document.getElementById("word").innerHTML = word[RandomArray]
         document.getElementById("뜻").innerHTML = "뜻"
 
         // 이미 한번 본 영단어 삭제
@@ -38,7 +41,7 @@ function 다시하기()
 // 뜻 보여주는 함수 
 function 뜻보여주기(){
     if(document.getElementById("뜻").innerText != "다 봤습니다"){
-        document.getElementById("뜻").innerHTML = number[RandomArray]
+        document.getElementById("뜻").innerHTML = 영단어구분[1]
         number.splice(RandomArray, 1)
     }
 }
